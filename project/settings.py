cf3f2c97bd2e5fefed05636d12b30f27b3251dc7
Alexander <51765464+alechries@users.noreply.github.com>
2021-03-19 11:04:40 +0200

    Update

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DynamicFormsetDjango.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {

a2cc803d972ef72b9f4c81085a887de863bed22d
Alexander <51765464+alechries@users.noreply.github.com>
2021-03-21 17:46:11 +0200

    Update

--- static/js/dynamicforms.js
+++ static/js/dynamicforms.js
@@ -1,3 +1,11 @@
+function updateFormIndex(element, prefix, index) {
+    let id_regex = new RegExp('(' + prefix + '-\\d+)');
+	let replacement = prefix + '-' + i;
+	let name = $(this).attr('name').replace(id_regex,replacement);
+	let id = 'id_' + name;
+    $(this).attr({'name': name, 'id': id}).val('');
+}
+
 function delForm(btn, prefix, form, counter) {
     $(btn).parents(form).remove();
     let forms = $(form);
@@ -6,13 +14,9 @@ function delForm(btn, prefix, form, counter) {
     for (var i=0, formCount=forms.length; i<formCount; i++) {
         element = $(forms.get(i));
         element.find(':input').each(function() {
-            let id_regex = new RegExp('(' + prefix + '-\\d+)');
-		    let replacement = prefix + '-' + i;
-		    let name = $(this).attr('name').replace(id_regex,replacement);
-		    let id = 'id_' + name;
-             $(this).attr({'name': name, 'id': id}).val('');
+            updateFormIndex(element, prefix, i)
         });
-        element.find(counter).text(i+1);
+        if (counter) element.find(counter).text(i+1);
     }
     return false;
 }

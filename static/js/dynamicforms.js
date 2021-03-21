638f4e4450d34da4df82c4da4c8e6a7434e68c93
Alexander <51765464+alechries@users.noreply.github.com>
2021-03-21 16:36:27 +0200

    Update

--- static/js/dynamicforms.js
+++ static/js/dynamicforms.js
@@ -2,14 +2,14 @@ function delForm(btn, selector, type) {
     $(btn).parents(selector).remove();
     var forms = $(selector);
     $('#id_' + type + '-TOTAL_FORMS').val(forms.length);
-
     for (var i=0, formCount=forms.length; i<formCount; i++) {
         element = $(forms.get(i));
         element.find(':input').each(function() {
             var id_regex = new RegExp('(' + type + '-\\d+)');
 		    var replacement = type + '-' + i;
-            var name = $(this).attr('name', ).replace(id_regex,replacement);
-            var id = 'id_' + name;
+		    var name = $(this).attr('name').replace(id_regex,replacement);
+		    var id = 'id_' + name;
+             $(this).attr({'name': name, 'id': id}).val('');
         });
         element.find('.addition-counter').text(i+1);
     }

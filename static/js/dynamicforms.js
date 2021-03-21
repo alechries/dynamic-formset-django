10d85dd7c5a3d8f0ce2052e44ba34a7d17ce2c55
Alexander <51765464+alechries@users.noreply.github.com>
2021-03-21 17:21:21 +0200

    Update

--- static/js/dynamicforms.js
+++ static/js/dynamicforms.js
@@ -17,23 +17,27 @@ function delForm(btn, selector, type) {
 }
 
 function addForm(selector, type) {
-    var newElement = $(selector).clone(true);
-    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    let newElement = $(selector).clone(true);
+    let total = $('#id_' + type + '-TOTAL_FORMS').val();
 
     newElement.find(':input').each(function() {
-        var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
-        var id = 'id_' + name;
+        let name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+        let id = 'id_' + name;
         $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
     });
 
     total++;
     $('#id_' + type + '-TOTAL_FORMS').val(total);
-    newElement.find('.addition-counter').text(total);
-    $(selector).after(newElement);
+
+    newElement.find('.addition-counter').each(function() { $(this).text(total); });
+
+
 
     newElement.find('.del-row').click(function() {
         return delForm(this, '.addition-form-part', 'addition');
     });
+
+    $(selector).after(newElement);
 
     height = document.body.scrollHeight;
     window.scrollTo(0, height);
